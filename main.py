758d1640181b4c092e1912b865a23792ce05b6d2
MaiconDante <maicondante@hotmail.com>
2026-06-01 14:42:05 -0300

    feat: Adiciona primeira janela PyQt6.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Arquivo principal da aplicação
+
+# Importando Biblioteca e Classes
+from PyQt6.QtWidgets import QApplication, QWidget
+
+app = QApplication([])
+
+window = QWidget()
+# Define o título da janela
+window.setWindowTitle("Sistema de Cadastro de Imóveis da Imobiliária")
+# Define (LarguraxAltura) da janela
+window.resize(800, 600)
+# Exibi a janela
+window.show()
+
+app.exec()
