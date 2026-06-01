0f5f1e89f741bd701634135f44547c4057f91fec
MaiconDante <maicondante@hotmail.com>
2026-06-01 16:37:14 -0300

    feat: Adiciona layout vertical e labels iniciais.

--- windows/WindowMain.py
+++ windows/WindowMain.py
@@ -1,4 +1,8 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QLabel
+)
 
 class MainWindow(QWidget):
 
@@ -9,3 +13,15 @@ class MainWindow(QWidget):
         self.setWindowTitle("Sistema de Cadastro de Imóveis da Imobiliária")
         # Define (LarguraxAltura) da janela
         self.resize(800, 600)
+        # Cria um organizador vertical
+        layout = QVBoxLayout()
+        # Texto que será exibido dentro da janela - [CRIA O COMPONENTE]
+        titulo = QLabel("Cadastro de Imóveis")
+        informacao = QLabel("Sistema desenvolvido em PyQt6")
+        versao = QLabel("Versão 1.0")
+        # Adiciona o texto na janela - [COLOCA O COMPONENTE NA TELA]
+        layout.addWidget(titulo)
+        layout.addWidget(informacao)
+        layout.addWidget(versao)
+        # Aqui diz esta janela usará este layout
+        self.setLayout(layout)
