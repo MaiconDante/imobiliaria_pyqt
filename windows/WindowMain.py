5dfe147a4fabe7b0bfef537808b902b29c1c653d
MaiconDante <maicondante@hotmail.com>
2026-06-02 09:58:34 -0300

    feat e refactor: Adiciona botão salvar e evento de clique e torna campos de entrada acessíveis pela classe.

--- windows/WindowMain.py
+++ windows/WindowMain.py
@@ -2,7 +2,8 @@ from PyQt6.QtWidgets import (
     QWidget,
     QVBoxLayout,
     QLabel,
-    QLineEdit
+    QLineEdit,
+    QPushButton
 )
 
 class MainWindow(QWidget):
@@ -12,32 +13,45 @@ class MainWindow(QWidget):
 
         # Define o título da janela
         self.setWindowTitle("Sistema de Cadastro de Imóveis da Imobiliária")
-        # Define (LarguraxAltura) da janela
+        # Define (Largura x Altura) da janela
         self.resize(800, 600)
         # Cria um organizador vertical
         layout = QVBoxLayout()
-        # Texto que será exibido dentro da janela - [CRIA O COMPONENTE]
+        # [LABELS] Texto que será exibido dentro da janela - [CRIA O COMPONENTE]
         titulo = QLabel("Cadastro de Imóveis")
-        layout.addWidget(titulo)
         label_codigo = QLabel("Código do Imóvel")
-        input_codigo = QLineEdit()
-        input_codigo.setPlaceholderText("Digite o código do Imóvel")
-        layout.addWidget(label_codigo)
-        layout.addWidget(input_codigo)
         label_endereco = QLabel("Endereço do Imóvel")
-        input_endereco = QLineEdit()
-        input_endereco.setPlaceholderText("Digite o endereço do Imóvel")
-        layout.addWidget(label_endereco)
-        layout.addWidget(input_endereco)
         label_cidade = QLabel("Cidade do Imóvel")
-        input_cidade = QLineEdit()
-        input_cidade.setPlaceholderText("Digite a cidade do Imóvel")
-        layout.addWidget(label_cidade)
-        layout.addWidget(input_cidade)
         informacao = QLabel("Sistema desenvolvido em PyQt6")
         versao = QLabel("Versão 1.0")
+        # [INPUTS] Campos de texto para usuário digitar, que será exibido dentro da janela - [CRIA O COMPONENTE]
+        self.input_codigo = QLineEdit()
+        self.input_codigo.setPlaceholderText("Digite o código do Imóvel")
+        self.input_endereco = QLineEdit()
+        self.input_endereco.setPlaceholderText("Digite o endereço do Imóvel")
+        self.input_cidade = QLineEdit()
+        self.input_cidade.setPlaceholderText("Digite a cidade do Imóvel")
+        # [BUTTONS] Botões de ação
+        btn_salvar = QPushButton("Salvar Imóvel")
+        btn_salvar.clicked.connect(self.salvar_imovel)
         # Adiciona o texto na janela - [COLOCA O COMPONENTE NA TELA]
+        layout.addWidget(titulo)
+        layout.addWidget(label_codigo)
+        layout.addWidget(self.input_codigo)
+        layout.addWidget(label_endereco)
+        layout.addWidget(self.input_endereco)
+        layout.addWidget(label_cidade)
+        layout.addWidget(self.input_cidade)
         layout.addWidget(informacao)
         layout.addWidget(versao)
+        layout.addWidget(btn_salvar)
         # Aqui diz esta janela usará este layout
         self.setLayout(layout)
+
+    def salvar_imovel(self):
+        texto_atual = self.windowTitle()
+
+        if texto_atual == "Sistema de Cadastro de Imóveis da Imobiliária":
+            self.setWindowTitle("Clique Detectado")
+        else:
+            self.setWindowTitle("Sistema de Cadastro de Imóveis da Imobiliária")
